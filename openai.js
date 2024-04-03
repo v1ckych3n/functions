20d4f5af9c0c1398e74c1afcb477dfda14799ad9
v1ckych3n <chenv159@newschool.edu>
2024-04-02 22:33:34 -0400

    Update openai.js

--- openai.js
+++ openai.js
@@ -25,6 +25,9 @@
 //   return <span></span>
 // }
 
+
+
+
 // const { Configuration, OpenAIApi } = require("openai")
 // const configuration = new Configuration({
 //   apiKey: process.env.OPENAI_API_KEY
