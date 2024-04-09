e543bf4b6682c513b3eee9ecaab419d09b10f0d0
v1ckych3n <chenv159@newschool.edu>
2024-04-09 12:49:14 -0400

    celebration works!

--- script/celebration.js
+++ script/celebration.js
@@ -1,3 +1,4 @@
+// THIS IS ONLY FOR CLICKING A BUTTON *need to make it where it can be drag and drop and then confetti explosion :)
 // const confettiButton = document.querySelector('#confettiButton');
 // const canvas = document.querySelector('#confetti');
 
@@ -7,7 +8,15 @@
 //     jsConfetti.addConfetti()
 // })
 
-// confettiButton.addEventListener("dragenter", dragEnter);
+// THIS WORKS FOR DRAG AND DROP AND THEN .... CONFETTI! //
+const celebrationTime = document.querySelector('#celebrationTime');
+const jsConfetti = new JSConfetti();
 
+celebrationTime.addEventListener('dragover', (event) => {
+    event.preventDefault();
+});
 
+celebrationTime.addEventListener('drop', () => {
+    jsConfetti.addConfetti()
+});
 
