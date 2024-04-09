22ec748fa6a3ff2f50fdb3926643fc7d3313d453
v1ckych3n <chenv159@newschool.edu>
2024-04-09 17:10:51 -0400

    connecting the json into javascript

--- script/motivational-buddy.js
+++ script/motivational-buddy.js
@@ -1,23 +1,28 @@
 const renderQuotes = (data) => {
-    const motivationalQuotes = document.getElementById("motivation-quotes");
+    const motivationalQuotes = document.getElementById("motivation-quotes")
 
-    data.forEach(item) => let conditionalClass = ''
+    data.forEach((item) => {
+        let conditionalClass = ''
 
-    // need to look up what !item.alsoWasWriter //
-    if (!item.alsoWasQuote) {
-        conditionalClass = 'none';
-    }
-
-    let listItem =
+        // need to look up what !item.alsoWasWriter //
+        if (!item.alsoWasQuote) {
+            conditionalClass = 'none';
+        }
+        
+        let listItem =
         `
             <li class="${conditionalClass}>
                 <p class="motivational-buddy_quotes">{$item.quote}</p>
             </li>
         `
     
-    motivationalQuotes.insertAdjacentHTML('beforeend', listItem);
+        motivationalQuotes.insertAdjacentHTML('beforeend', listItem)
+    })
 }
 
 
 fetch('motivational-buddy.json')
-    .then(response)
+    .then(response => response.json())
+    .then(data => {
+        renderItems(data)
+    })
