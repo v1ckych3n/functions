2b913e0174352347de734227d9632eeeb350c6fd
v1ckych3n <chenv159@newschool.edu>
2024-04-09 17:04:05 -0400

    updating the javascript for the quotes to show

--- script/motivational-buddy.js
+++ script/motivational-buddy.js
@@ -1,5 +1,23 @@
 const renderQuotes = (data) => {
     const motivationalQuotes = document.getElementById("motivation-quotes");
 
+    data.forEach(item) => let conditionalClass = ''
+
+    // need to look up what !item.alsoWasWriter //
+    if (!item.alsoWasQuote) {
+        conditionalClass = 'none';
+    }
+
+    let listItem =
+        `
+            <li class="${conditionalClass}>
+                <p class="motivational-buddy_quotes">{$item.quote}</p>
+            </li>
+        `
     
+    motivationalQuotes.insertAdjacentHTML('beforeend', listItem);
 }
+
+
+fetch('motivational-buddy.json')
+    .then(response)
