d193a34354cdd8ac3448c848e2d60c9d81572de1
v1ckych3n <chenv159@newschool.edu>
2024-04-10 09:17:58 -0400

    Update motivational-buddy.js

--- script/motivational-buddy.js
+++ script/motivational-buddy.js
@@ -1,29 +1,30 @@
-const renderQuotes = (data) => {
-    const motivationalQuotes = document.getElementById("motivational-quotes")
+// const renderQuotes = (data) => {
+//     const motivationalQuotes = document.getElementById("motivational-quotes")
 
-    data.forEach((item) => {
-        let conditionalClass = ''
+//     data.forEach((item) => {
+//         let conditionalClass = ''
 
-        // need to look up what !item.alsoWasWriter //
-        if (!item.alsoWasQuote) {
-            conditionalClass = 'none';
-        }
+//         // need to look up what !item.alsoWasWriter //
+//         if (!item.quote) {
+//             conditionalClass = 'none';
+//         }
         
-        let listItem =
-        `
-            <li class="${conditionalClass}>
-                <p class="motivational-buddy_quotes">{$item.quote}</p>
-            </li>
-        `
+//         let listItem =
+//         `
+//             <li class="${conditionalClass}>
+//                 <p class="motivational-buddy_quotes">{$item.quote}</p>
+//             </li>
+//         `
     
-        motivationalQuotes.insertAdjacentHTML('beforeend', listItem)
-    })
-}
+//         motivationalQuotes.insertAdjacentHTML('beforeend', listItem)
+//     })
+// }
 
 
-fetch('/script/motivational-buddy.json')
-    .then(response => response.json())
-    .then(data => {
-        renderItems(data)
-    })
-    
+// fetch('/script/motivational-buddy.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         renderItems(data)
+//     })
+
+
