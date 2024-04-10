5f2f3045d2505771f1c3b51ae3dc4f999176ebd2
v1ckych3n <chenv159@newschool.edu>
2024-04-10 10:04:11 -0400

    json

--- script/motivational-buddy.js
+++ script/motivational-buddy.js
@@ -5,7 +5,7 @@
 //         let conditionalClass = ''
 
 //         // need to look up what !item.alsoWasWriter //
-//         if (!item.quote) {
+//         if (!item.alsoWasWriter) {
 //             conditionalClass = 'none';
 //         }
         
@@ -20,11 +20,46 @@
 //     })
 // }
 
+const renderItems = (data) => {
+	// The `ul` where the items will be inserted
+	const dataList = document.getElementById('motivational-quotes')
 
-// fetch('/script/motivational-buddy.json')
-//     .then(response => response.json())
-//     .then(data => {
-//         renderItems(data)
-//     })
+	// Loop through each item in the data array
+	data.forEach((item) => {
+		let conditionalClass = '' // Set an empty class variable
+
+		if (!item.alsoWasWriter) { // Conditional if this is `false` (“not true”)
+			conditionalClass = 'faded' // Update the variable
+		}
+
+		// Make a “template literal” as we have before, inserting your data (and maybe the class)
+		let listItem =
+			`
+				<li class="${conditionalClass}">
+					<h2>${item.title}</h2>
+					<img src="${item.posterImage}">
+					<p>Released in <time>${item.year}</time></p>
+					<p><em>${item.runTime}</em></p>
+					<a href="${item.imdbLink}">
+						<p>${item.imdbRating} / 10 →</p>
+					</a>
+				</li>
+			`
+
+		dataList.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
+	})
+}
 
 
+fetch('script/motivational-buddy.json')
+    .then(response => response.json())
+    .then(data => {
+        renderItems(data);
+        console.log(data);
+    });
+
+
+// MAKE MY BUDDY MOVE //
+dragElement(document.getElementById ("moving-buddy"));
+
+function dragElement(mov)
