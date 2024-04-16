8456949e77ef629497b6573ee6120a9681aff635
v1ckych3n <chenv159@newschool.edu>
2024-04-16 12:00:52 -0400

    setting up the motivational quotes

--- script/motivational-buddy.js
+++ script/motivational-buddy.js
@@ -1,26 +1,32 @@
-const renderItems = (data) => {
-	// The `ul` where the items will be inserted
-	const dataList = document.getElementById('motivational-quotes')
-
-	// Loop through each item in the data array
-	data.forEach((item) => {
-		let conditionalClass = '' // Set an empty class variable
-
-		if (!item.alsoWasWriter) { // Conditional if this is `false` (“not true”)
-			conditionalClass = 'faded' // Update the variable
-		}
-
-		// Make a “template literal” as we have before, inserting your data (and maybe the class)
-		let listItem =
-        `
-            <li class="${conditionalClass}>
-                <p class="motivational-buddy_quotes">${item.quote}</p>
-            </li>
-        `
-
-		dataList.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
-	})
+const renderQuote = (data) => {
+	
 }
+
+
+// MICHAEL'S VERSION //
+// const renderItems = (data) => {
+// 	// The `ul` where the items will be inserted
+// 	const quoteElement = document.getElementById('motivational-quotes');
+
+// 	// Loop through each item in the data array
+// 	data.forEach((item) => {
+// 		let conditionalClass = '' // Set an empty class variable
+
+// 		if (!item.alsoWasWriter) { // Conditional if this is `false` (“not true”)
+// 			conditionalClass = 'faded' // Update the variable
+// 		}
+
+// 		// Make a “template literal” as we have before, inserting your data (and maybe the class)
+// 		let listItem =
+//         `
+//             <li class="${conditionalClass}>
+//                 <p class="motivational-buddy_quotes">${item.quote}</p>
+//             </li>
+//         `
+
+// 		dataList.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
+// 	})
+// }
 
 
 fetch('script/motivational-buddy.json')
