274ab6c16bfc49d54c9da6f2c3b662f64bf5005c
v1ckych3n <chenv159@newschool.edu>
2024-04-16 16:31:24 -0400

    styling the motivational buddy

--- script/motivational-buddy.js
+++ script/motivational-buddy.js
@@ -1,48 +1,49 @@
-const renderQuote = (data) => {
-	const quoteElement = document.getElementById('motivational-quotes');
+// const renderItems = (data) => {
+// 	const quoteElement = document.getElementById('motivational-quotes');
 
-	if (data.length > 0) {
-		const generateQuote = Math.floor(Math.random() * data.length);
-		const selectedQuote = data[randomIndex];
+// 	if (data.length > 0) {
+// 		const generateQuote = Math.floor(Math.random() * data.length);
+// 		const selectedQuote = data[randomIndex];
 
-		let conditionalClass = '';
-		if (!item.quote) {
-			conditionalClass ='faded'
-		}
+// 		let conditionalClass = '';
+// 		if (!item.quote) {
+// 			conditionalClass ='faded'
+// 		}
 
-		quoteElement.innerHTML = 
-			`
-				<li class="${conditionalClass}">
-					<p class="motivational-buddy_quotes">${item.quote}</p>
-			`
-	}
-}
+// 		quoteElement.innerHTML = 
+// 			`
+// 				<li class="${conditionalClass}">
+// 					<p class="motivational-buddy_quotes">${item.quote}</p>
+// 				</li>
+// 			`;
+// 	}
+// };
 
 
 // MICHAEL'S VERSION //
-// const renderItems = (data) => {
-// 	// The `ul` where the items will be inserted
-// 	const quoteElement = document.getElementById('motivational-quotes');
+const renderItems = (data) => {
+	// The `ul` where the items will be inserted
+	const quoteElement = document.getElementById('motivational-quotes');
 
-// 	// Loop through each item in the data array
-// 	data.forEach((item) => {
-// 		let conditionalClass = '' // Set an empty class variable
+	// Loop through each item in the data array
+	data.forEach((item) => {
+		let conditionalClass = '' // Set an empty class variable
 
-// 		if (!item.alsoWasWriter) { // Conditional if this is `false` (“not true”)
-// 			conditionalClass = 'faded' // Update the variable
-// 		}
+		if (!item.alsoWasWriter) { // Conditional if this is `false` (“not true”)
+			conditionalClass = 'faded' // Update the variable
+		}
 
-// 		// Make a “template literal” as we have before, inserting your data (and maybe the class)
-// 		let listItem =
-//         `
-//             <li class="${conditionalClass}>
-//                 <p class="motivational-buddy_quotes">${item.quote}</p>
-//             </li>
-//         `
+		// Make a “template literal” as we have before, inserting your data (and maybe the class)
+		let listItem =
+        `
+            <li class="${conditionalClass}>
+                <p class="motivational-buddy_quotes">${item.quote}</p>
+            </li>
+        `
 
-// 		dataList.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
-// 	})
-// }
+		dataList.insertAdjacentHTML('beforeend', listItem) // Add it to the `ul`!
+	})
+}
 
 
 fetch('script/motivational-buddy.json')
